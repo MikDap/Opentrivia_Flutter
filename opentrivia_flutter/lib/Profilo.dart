694d2eeb65c854ffb00057aad17ce9c6e5f0b60c
MikDap <dpmiky18@gmail.com>
2024-02-12 12:02:52 +0100

    Merge 12/02

---
+++ opentrivia_flutter/lib/Profilo.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:math';
+class Profilo extends StatefulWidget {
+@override
+_ProfiloState createState() => _ProfiloState();
+}
+class _ProfiloState extends State<Profilo> {
+final databaseReference = FirebaseDatabase.instance.ref();
+late String uid;
+String nomeUtente = '';
+@override
+void initState(){
+super.initState();
+uid = FirebaseAuth.instance.currentUser!.uid;
+retrieveUsername(); // Richiama la funzione per recuperare il nome utente quando il widget viene creato
+}
+// Funzione per recuperare il nome utente dal database Firebase
+Future<void> retrieveUsername() async {
+var usersRef = databaseReference.child('users').child(uid);
+final user = await usersRef.once();
+DataSnapshot leggiuser = user.snapshot;
+setState(() {
+int randomNumber = generateRandomNumber();
+if(leggiuser.child('name').value != '') {
+nomeUtente = leggiuser
+    .child('name')
+    .value.toString();
+}
+else { nomeUtente = 'user$randomNumber';
+usersRef.child('name').set(nomeUtente);
+}
+});
+}
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text('Profilo'),
+),
+body: nomeUtente.isNotEmpty // Controlla se il nome utente è stato recuperato con successo
+? Center(
+child: Text(
+'Benvenuto, $nomeUtente!',
+style: TextStyle(fontSize: 20),
+),
+)
+    : Center(
+child: CircularProgressIndicator(), // Visualizza un indicatore di caricamento se il nome utente è in fase di recupero
+),
+);
+}
+int generateRandomNumber() {
+Random random = Random();
+return random.nextInt(10000);
+}
+}
---
+++ opentrivia_flutter/lib/lista_utenti/ListaUtenti.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Utente {
+  final String id;
+  final String name;
+
+  Utente(this.id, this.name);
+}
+
+class ListaUtenti extends StatefulWidget {
+  @override
+  _ListaUtentiState createState() => _ListaUtentiState();
+}
+
+class _ListaUtentiState extends State<ListaUtenti> {
+  late List<Utente> listaUtenti;
+  late String uid;
+  late DatabaseReference usersRef;
+
+  @override
+  void initState() {
+    super.initState();
+    listaUtenti = [];
+    uid = FirebaseAuth.instance.currentUser!.uid;
+    usersRef = FirebaseDatabase.instance.ref().child('users');
+
+    _caricaUtenti();
+  }
+
+  Future<void> _caricaUtenti() async {
+    try {
+      // Ottieni la lista degli utenti dal database Firebase
+      final users = await usersRef.once();
+      DataSnapshot listaUsers = users.snapshot;
+
+      // Verifica se ci sono dati nell'elenco degli utenti
+      if (listaUsers.exists) {
+        // Converte il valore in un tipo Map
+        Map<dynamic, dynamic>? usersMap = listaUsers.value as Map<dynamic, dynamic>?;
+
+        // Verifica se usersMap non è nullo
+        if (usersMap != null) {
+          // Itera su ogni utente nel DataSnapshot
+          usersMap.forEach((key, value) {
+            // Crea un nuovo oggetto Utente e aggiungilo alla lista
+            Utente utente = Utente(key, value['name']);
+            // Aggiungi l'utente alla listaUtenti solo se l'ID non corrisponde all'ID dell'utente corrente
+            if (utente.id != uid) {
+              setState(() {
+                listaUtenti.add(utente);
+              });
+            }
+          });
+        }
+      }
+    } catch (error) {
+      print("Errore durante il recupero degli utenti: $error");
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const  Text('LISTA UTENTI'),
+      ),
+      body: ListView.builder(
+        itemCount: listaUtenti.length,
+        itemBuilder: (context, position) {
+          var utente = listaUtenti[position];
+          if (utente.id != uid) {
+            return ListTile(
+              title: Text(
+                utente.name,
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text(
+                'ID: ${utente.id}',
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
+}
